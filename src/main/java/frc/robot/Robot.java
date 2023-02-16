8ea2e4e1f5eab263ed8cc40a299161735df6380d
robbie aldrich <robbiemaldrich@gmail.com>
2023-02-15 22:07:50 -0600

    first commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+// don't fuck with the class name lol
+public class Robot extends TimedRobot {
+
+
+
+  @Override
+  public void robotInit() { }
+
+  @Override
+  public void robotPeriodic() { }
+
+
+
+
+
+
+
+  @Override
+  public void autonomousInit() { }
+
+  @Override
+  public void autonomousPeriodic() { }
+
+
+
+
+
+
+  @Override
+  public void teleopInit() { }
+
+  @Override
+  public void teleopPeriodic() { }
+
+
+
+
+
+  @Override
+  public void disabledInit() { }
+
+  @Override
+  public void disabledPeriodic() { }
+
+
+
+
+  @Override
+  public void testInit() { }
+
+  @Override
+  public void testPeriodic() { }
+}
